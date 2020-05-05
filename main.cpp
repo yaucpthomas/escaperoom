6b1ca8f66753f95b5d6c71d91c8dca38be674920
Thomas Yau <yaucp@connect.hku.hk>
2020-05-05 17:36:52 +0800

    update start scene dialogue

--- main.cpp
+++ main.cpp
@@ -32,6 +32,9 @@ int main()
     StartScene();
   }
   else {
+    string file_loc;
+    cout<<"What is the name of your file? (It must inlude .txt at the end.)";
+    cin>>file_loc;
 
   }
 
@@ -62,15 +65,35 @@ void StartNewGame(Player& player){
 }
 
 void StartScene(){
-  cout<<"(Door knocked)\n"
-      <<"Who’s there?\n"
-      <<"Oleg. Oleg Penkovsky. Джордж Кисевальтер (countersign)\n"
-      <<"Come in first, my pal. (Door opens) Things go right and we're gonna leave as soon...\n"
-      <<"Popov, you bastard. Caught him right now!\n"
-      <<"Wait, what? Not Oleg?? (How come KGB knows the sign...)\n"
-      <<"Man, you’re so damn bold, huh? How dare you betray your own country? (Pager rings) Sir, we got em. We’ll return now. (Hang up the call) Bring him to the car and give him the taste of pain.\n"
-      <<"No god! Please no!!! NOOO… (Fainted by head hit)\n"
-      <<"\n[Press enter to continue]\n";
-  cin.ignore();
+  cout<<"(Door knocked)\n";
+  this_thread::sleep_for (chrono::seconds(2));
+  cout<<"Who’s there?\n";
+  this_thread::sleep_for (chrono::seconds(2));
+  cout<<"Oleg. Oleg Penkovsky. Джордж Кисевальтер (countersign)\n";
+  this_thread::sleep_for (chrono::seconds(3));
+  cout<<"Come in first, my pal. (Door opens) Things go right and we're gonna leave as soon...\n";
+  this_thread::sleep_for (chrono::seconds(5));
+  cout<<"Popov, you bastard. Caught him right now!\n";
+  this_thread::sleep_for (chrono::seconds(4));
+  cout<<"Wait, what? Not Oleg?? (How come KGB knows the sign...)\n";
+  this_thread::sleep_for (chrono::seconds(4));
+  cout<<"Man, you’re so damn bold, huh? How dare you betray your own country?\n";
+  this_thread::sleep_for (chrono::seconds(5));
+  cout<<"(Pager rings) Sir, we got em. We’ll return now. (Hang up the call)\n";
+  this_thread::sleep_for (chrono::seconds(5));
+  cout<<"Bring him to the car and give him the taste of pain.\n";
+  this_thread::sleep_for (chrono::seconds(4));
+
+  cout<<"No god! Please no!!! NOOO… (Fainted by head hit)\n";
+  this_thread::sleep_for (chrono::seconds(4));
+  cout<<".\n";
+  this_thread::sleep_for (chrono::seconds(2));
+  cout<<".\n";
+  this_thread::sleep_for (chrono::seconds(2));
+  cout<<".\n";
+
+  cout<<"\nYou wake up on the cold floor.\n"
+      <<"You sit up and realize that your body is seriously bruised.\n"
+      <<"Your feet are also manacled.\n";
 
 }
